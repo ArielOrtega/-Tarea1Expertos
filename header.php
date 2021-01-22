1fd5cd68f0dcd029881a9969e03f66af02f2499a
ArielOrtega <arielortegabrenes98@gmail.com>
2021-01-21 20:45:16 -0600

    Try fix header links

--- header.php
+++ header.php
@@ -1,16 +1,16 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">=
         <div class="navbar-header">
-            <a class="navbar-brand active" href="/codigo/Tarea1Expertos/index.php">Tarea 1</a>
+            <a class="navbar-brand active" href="index.php">Tarea 1</a>
         </div>
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item"><a class="nav-link" href="/codigo/Tarea1Expertos/src/formTarea1.php">Estilo de aprendizaje</a></li>
-                <li class="nav-item"><a class="nav-link" href="/codigo/Tarea1Expertos/src/formRecinto.php">Recinto de origen</a></li>
-                <li class="nav-item"><a class="nav-link" href="/codigo/Tarea1Expertos/src/formSexo.php">Sexo de estudiante</a></li>
-                <li class="nav-item"><a class="nav-link" href="/codigo/Tarea1Expertos/src/formEstilo.php">Aprendizaje y recinto</a></li>
-                <li class="nav-item"><a class="nav-link" href="/codigo/Tarea1Expertos/src/formProfesor.php">Tipo de profesor</a></li>
-                <li class="nav-item"><a class="nav-link" href="/codigo/Tarea1Expertos/src/formRedes.php">Clasificacion de redes</a></li>
+                <li class="nav-item"><a class="nav-link" href="../src/formTarea1.php">Estilo de aprendizaje</a></li>
+                <li class="nav-item"><a class="nav-link" href="../src/formRecinto.php">Recinto de origen</a></li>
+                <li class="nav-item"><a class="nav-link" href="../src/formSexo.php">Sexo de estudiante</a></li>
+                <li class="nav-item"><a class="nav-link" href="../src/formEstilo.php">Aprendizaje y recinto</a></li>
+                <li class="nav-item"><a class="nav-link" href="../src/formProfesor.php">Tipo de profesor</a></li>
+                <li class="nav-item"><a class="nav-link" href="../src/formRedes.php">Clasificacion de redes</a></li>
             </ul>
         </div>
     </nav>
